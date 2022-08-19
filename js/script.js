0ab57a94e283acb00905c09f458de67ec25999d7
Kelsy <109422588+KelsyAlyssa@users.noreply.github.com>
2022-08-19 10:27:02 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+var button = document.querySelector(".show-cat");
+var cat = document.querySelector(".cat");
+
+// console.log(button);
+// console.log(cat);
+
+button.addEventListener("click", function () {
+  if (cat.classList.contains("show")) {
+    // console.log("yes");
+    cat.classList.remove("show");
+    button.innerText = "Wait, Come back!";
+    button.classList.add("disappear");
+  } else {
+    // console.log("no");
+    cat.classList.add("show");
+    button.innerText = "Shoo, cat!!";
+    button.classList.remove("disappear");
+  }
+});
